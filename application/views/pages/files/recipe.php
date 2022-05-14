bd23154fc961e8494b8350d5db3ab52363ac21cf
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2022-05-13 20:04:50 -0500

    Charset

--- application/views/pages/files/recipe.php
+++ application/views/pages/files/recipe.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<head><meta http-equiv="Content-Type" content="text/html; charset=big5">
     <meta http-equiv="content-language" content="es" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
--- application/views/pages/files/recipeOut.php
+++ application/views/pages/files/recipeOut.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=big5" />
     <meta http-equiv="content-language" content="es" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
